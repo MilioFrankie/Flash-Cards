02c0acda3ade76403de1dcb1715cca577b659514
Emilio <em.francomartinez@gmail.com>
2018-12-03 23:12:33 -0700

    updated index.js, css and html

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-var deck = []
+var newQ = document.getElementById("question");
+var newA = document.getElementById("answer");
+var newCard = document.getElementById("createCard");
 
-var
+newCard.addEventListener("click", setCard);
+
+var deck = [];
+
+function setCard() {
+  var new_question = newQ.value;
+  var new_answer = newA.value;
+
+  document.getElementById("cTitle").innerHTML = new_question;
+}
